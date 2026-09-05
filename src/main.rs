da660f456356b101c51da57e97bb0169b4e1c6b2
Manish-N-G-Zuko <136582364+Manish-N-G@users.noreply.github.com>
2026-09-06 01:35:26 +0200

    Update pics upto fmt for presentation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rand::*;
+
 fn main() {
-    println!("Hello, world!");
+    // entry point
+    let chick1 = Chicken::new();
+    let chick2 = Chicken::new();
+
+    let rex1 = TRex::new();
+}
+
+struct Chicken {
+    health: f32,
+    attack: f32,
+}
+
+struct TRex {
+    health: f32,
+    attack: f32,
+}
+
+impl Chicken {
+    fn new() -> Chicken {
+        Chicken {
+            health: random_range(8.0..12.0),
+            attack: random_range(0.5..2.5),
+        }
+    }
+}
+
+impl TRex {
+    fn new() -> Self {
+        Self {
+            health: random_range(1500.0..2000.0),
+            attack: random_range(300.0..500.0),
+        }
+    }
 }
