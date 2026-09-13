9285a9896f279a547d375e16b3cca1bc193a0d6d
Manish-N-G-Zuko <136582364+Manish-N-G@users.noreply.github.com>
2026-09-14 00:31:51 +0200

    Update main function to use the lib

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,17 @@ use chick_vs_rex::*;
 use std::env;
 
 fn main() {
+    // entry point
     let args: Vec<String> = env::args().skip(1).collect();
 
-    // let result = match prepare_fight( args ) {
-    //     Ok(res) => res,
-    //     Err(e) => {
-    //         eprintln!("{e}");
-    //         std::process::exit(1);
-    //     },
-    // };
+    let result = match prepare_fight( args ) {
+        Ok(res) => res,
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1);
+        },
+    };
 
-    // println!( "{result}" );
-
-    let x = (0..1_000_000).collect::<Vec<u128>>();
-    let val = std::time::Instant::now();
-    sum_of_sq( &x );
-    let val = val.elapsed();
-    println!("Sum of squares for 1,000,000:");
-    println!("time: {:?}", val);
-
-    let val = std::time::Instant::now();
-    sum_of_sq_rayon( &x );
-    let val = val.elapsed();
-    println!("time with rayon: {:?}", val);
+    println!( "{result}" );
 }
 
-
-
-
-fn sum_of_sq( input: &[u128] ) -> u128 {
-    input
-        .iter()
-        .map( |val| val*val )
-        .sum()
-}
-
-
-use rayon::prelude::*;
-// Parallel processing for Iterators
-fn sum_of_sq_rayon( input: &[u128] ) -> u128 {
-    input
-        .par_iter()
-        .map( |val| val*val )
-        .sum()
-}
